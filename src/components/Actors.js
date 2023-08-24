59ac888c8921ded85d52b5d0fc64a11e5fc86eec
myrinja <myrinjau98@gmail.com>
2023-08-24 13:42:46 +0300

    update all

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      <div>
+        {actors.map((actor, index) => (
+          <div key={index}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie, movieIndex) => (
+                <li key={movieIndex}>{movie}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Actors;
